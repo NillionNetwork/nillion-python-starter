d503ec4199ca436f29d2d2d27b2558f394a703c8
David Butler <davidtbutler@Davids-MacBook-Air.local>
2024-05-01 16:18:40 +0100

    Restructure directories and manually test everything works

--- compile_programs.sh
+++ compile_programs.sh
@@ -26,10 +26,10 @@ echo "Now try running an example:"
 
 echo "----------single party compute --------------"
 
-echo "Code for single party compute lives in the client_single_party_compute folder"
-echo "📋 to run single party compute - addition_simple program: 'cd client_single_party_compute && python3 addition_simple.py'"
+echo "Code for single party compute lives in the examples_and_tutorials/core_concept_client_single_party_compute folder"
+echo "📋 to run single party compute - addition_simple program: 'cd examples_and_tutorials/core_concept_client_single_party_compute && python3 addition_simple.py'"
 
 echo "----------multi party compute --------------"
 
-echo "Code for single party compute lives in the client_multi_party_compute folder"
-echo "📋 to run multi party compute in 3 steps - addition_simple_multi_party_3: 'cd client_multi_party_compute && python3 01_store_secret_party1.py'"
+echo "Code for multi party compute lives in the examples_and_tutorials/core_concept_multi_party_compute folder"
+echo "📋 to run multi party compute in 3 steps - addition_simple_multi_party_3: 'cd examples_and_tutorials/core_concept_multi_party_compute && python3 01_store_secret_party1.py'"
