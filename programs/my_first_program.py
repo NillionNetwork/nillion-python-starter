b4028ac883025b73f8ebdebaca9e2f7e990c90d0
Omkar-Ghongade <omkarsubhashghongade21@gmail.com>
2024-06-29 12:33:58 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,13 +1,11 @@
 from nada_dsl import *
 
 def lcm(n1, n2):
-    # Calculate the greatest common divisor (GCD) using Euclid's algorithm
     def lcm1(a, b):
         while b != Integer(0):
             a, b = b, a % b
         return a
 
-    # Calculate LCM using the relationship: LCM(a, b) = |a*b| / GCD(a, b)
     return lcm1(n1, n2)
 
 def nada_main():
