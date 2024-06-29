7ee2c5280f0129b2dd2056641a53d9c809a242bb
Omkar-Ghongade <omkarsubhashghongade21@gmail.com>
2024-06-29 12:31:35 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,22 @@
 from nada_dsl import *
 
+def lcm(n1, n2):
+    # Calculate the greatest common divisor (GCD) using Euclid's algorithm
+    def lcm1(a, b):
+        while b != Integer(0):
+            a, b = b, a % b
+        return a
+
+    # Calculate LCM using the relationship: LCM(a, b) = |a*b| / GCD(a, b)
+    return lcm1(n1, n2)
 
 def nada_main():
     party1 = Party(name="Party1")
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    # Securely compute the LCM using the lcm function
+    result = lcm(my_int1, my_int2)
 
-    return [Output(my_int1, "my_output", party1)]
+    # Output the result securely
+    return [Output(result, "my_output", party1)]
