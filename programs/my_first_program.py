2ea01abde862a545345d2803329b14e371cf5e01
Akshat0908 <114748641+Akshat0908@users.noreply.github.com>
2024-07-01 13:49:29 +0530

    my_first_program update.py

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,6 @@
 from nada_dsl import *
 
+nada_main = lambda: [Output(sum(SecretInteger(Input(f"int{i}", p := Party("P1"))) for i in "12"), "result", p)]
 
-def nada_main():
-    party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
-
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
-
-    return [Output(my_int1, "my_output", party1)]
+if __name__ == "__main__":
+    nada_main()
