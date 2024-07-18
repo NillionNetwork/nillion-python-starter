0b46a765ea90cb38ded382f345aa57112a8affca
Sriramkashyapks <kssriramkashyap002@gmail.com>
2024-07-18 18:41:40 +0000

    my new nada program

--- quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
+++ quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
@@ -1,12 +1,17 @@
 from nada_dsl import *
-def nada_main():
 
+def nada_main():
+    # Define the party
     party1 = Party(name="Party1")
 
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
+    # Input the string from the party
+    input_str = SecretString(Input(name="input_str", party=party1))
 
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    # Reverse the string
+    reversed_str = SecretString(input_str[::-1])
 
-    new_int = my_int1 + my_int2
+    # Check if the input string is equal to the reversed string
+    is_palindrome = SecretBoolean(input_str == reversed_str)
 
-    return [Output(new_int, "my_output", party1)]
+    # Return the result
+    return [Output(is_palindrome, "is_palindrome_output", party1)]
