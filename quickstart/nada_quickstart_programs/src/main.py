98687104c5014750a6fb6cba22d93ff857816f7e
Yashasyashu660 <yashasyashu6666@gmail.com>
2024-07-18 19:31:12 +0000

    my new nada program

--- quickstart/nada_quickstart_programs/src/main.py
+++ quickstart/nada_quickstart_programs/src/main.py
@@ -1,12 +1,28 @@
-from nada_dsl import *
+import random
 
-def nada_main():
-    party1 = Party(name="Party1")
-    party2 = Party(name="Party2")
-    party3 = Party(name="Party3")
-    a = SecretInteger(Input(name="A", party=party1))
-    b = SecretInteger(Input(name="B", party=party2))
+def guess_number():
+    # Generate a random number between 1 and 100
+    number_to_guess = random.randint(1, 100)
+    guess = None
+    attempts = 0
+    
+    print("I'm thinking of a number between 1 and 100. Try to guess it!")
 
-    result = a + b
+    while guess != number_to_guess:
+        try:
+            # Get the user's guess
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+            
+            if guess < number_to_guess:
+                print("Too low! Try again.")
+            elif guess > number_to_guess:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed the number in {attempts} attempts.")
+        
+        except ValueError:
+            print("Please enter a valid number.")
 
-    return [Output(result, "my_output", party3)]
+guess_number()
+
