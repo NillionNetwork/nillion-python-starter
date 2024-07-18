a13c5863354043624aa3f2d335be6df940245f5e
Antiparticlexe <hubGitacc@gmail.com>
2024-07-18 19:04:46 +0000

    my new nada program

---
+++ quickstart/client_code/run_my_first_program.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence[:n]
+
+# Example usage:
+n = 10  # Number of Fibonacci numbers to generate
+print(fibonacci(n))
