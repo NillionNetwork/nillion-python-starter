ca0417ff6b3b3d60257cf977802d8bbf1d40cb57
anshikaa23 <janshika2323@gmail.com>
2024-07-20 15:12:43 +0000

    my new nada program

--- quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
+++ quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
@@ -1,12 +1,23 @@
 from nada_dsl import *
+
 def nada_main():
 
     party1 = Party(name="Party1")
+    party2 = Party(name="Party2")
 
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    my_int3 = SecretInteger(Input(name="my_int3", party=party2))
 
-    new_int = my_int1 + my_int2
+    sum_int = my_int1 + my_int2
+    diff_int = my_int1 - my_int3
+    prod_int = my_int2 * my_int3
 
-    return [Output(new_int, "my_output", party1)]
+    max_int = Maximum([sum_int, diff_int, prod_int])
+
+    return [
+        Output(sum_int, "sum_output", party1),
+        Output(diff_int, "diff_output", party2),
+        Output(prod_int, "prod_output", party1),
+        Output(max_int, "max_output", party2)
+    ]
